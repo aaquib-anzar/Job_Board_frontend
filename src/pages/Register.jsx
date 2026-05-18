f7ddb41008716252f01c2d9fb8b8759f248b31aa
Aaquib <aaquib.anzar17@gmail.com>
2026-05-18 17:46:19 +0530

    fix:Typo is there in Resgister.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -28,7 +28,7 @@ export default function Register({ setUser }) {
 
   const handleSubmit = async (values, { setSubmitting, setErrors, resetForm }) => {
     try {
-      const res = await axios.post(`${BASE_URL}/auth/signup`, values, {
+      const res = await axios.post(`${BASE_URL}/auth/register`, values, {
         withCredentials: true,
       });
       toast.success("Registered Successfully")
